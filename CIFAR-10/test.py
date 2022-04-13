2ffc7aec7a5db7330c8434f320c26bb251dd5649
dwDavidxd <85202634+dwDavidxd@users.noreply.github.com>
2022-04-13 15:29:07 +0800

    Update test.py

--- CIFAR-10/test.py
+++ CIFAR-10/test.py
@@ -24,8 +24,6 @@ parser.add_argument('--widen_factor', default=20, type=int, help='width of model
 parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
 parser.add_argument('--dataset', default='cifar10adver'
                                          '', type=str, help='dataset = [cifar10/cifar100]')
-# parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-# parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"]='0,1,2,3'
@@ -37,18 +35,14 @@ best_acc = 0
 # Data Uplaod
 print('\n[Phase 1] : Data Preparation')
 
-# src_domain = './results/HGD/VGG/PGD/advu'
-# src_domain = './results/APE/PGD/advu'
-src_domain = './data/adv_example/test/VGG/FWA/advu_npy'
-label_dirs = './results/label_true_test.pkl'
+src_domain = './data/test/adv/PGD/npy'
+label_dirs = './data/test/label_true.pkl'
 
 
 batch_size = 500
 
 trans = transforms.Compose([
-    transforms.ToTensor(),
-    #transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
-])
+    transforms.ToTensor()])
 
 test_data = DatasetNPY(img_dirs=src_domain, label_dirs=label_dirs, transform=trans)
 test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, drop_last=True)
@@ -78,7 +72,7 @@ def getNetwork(args):
 print('\n[Test Phase] : Model setup')
 assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
 _, file_name = getNetwork(args)
-checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
+checkpoint = torch.load('./checkpoint/'+file_name+'.t7')
 net = checkpoint['net']
 
 if use_cuda:
@@ -96,18 +90,12 @@ with torch.no_grad():
     for batch_idx, (inputs, targets) in enumerate(test_loader):
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
+            
         inputs, targets = Variable(inputs), Variable(targets)
         outputs = net(inputs)
 
         _, predicted = torch.max(outputs.data, 1)
 
-        '''
-        print(predicted[7])
-        print(predicted[8])
-        print(predicted[9])
-        print(predicted[10])
-        print(predicted[11])
-        '''
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
 
@@ -115,4 +103,3 @@ with torch.no_grad():
 
     acc = 100.*correct/total
     print("| Test Result\tAcc@1: %.2f%%" %(acc))
-    print(100-acc)
